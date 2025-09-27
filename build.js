6bc219c7d5a397b020ee49b322d229f13c0ff21e
akcyp <bartosz.pycka.dev@gmail.com>
2025-09-28 00:50:43 +0200

    Fix build

---
+++ build.js
@@ -0,0 +1,36 @@
+const browserify = require('browserify');
+const uglify = require('uglify-js');
+const fs = require('fs');
+const path = require('path');
+
+const inputFile = 'src/index.js';
+const outputFile = 'dist/bundle.js';
+
+const outputDir = path.dirname(outputFile);
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir, { recursive: true });
+}
+
+const b = browserify(inputFile).transform('babelify');
+
+b.bundle((err, buf) => {
+  if (err) {
+    console.error('Browserify error:', err);
+    return;
+  }
+
+  const code = buf.toString();
+  const minified = uglify.minify(code);
+  if (minified.error) {
+    console.error('UglifyJS error:', minified.error);
+    return;
+  }
+
+  fs.writeFile(outputFile, minified.code, (writeErr) => {
+    if (writeErr) {
+      console.error('Write file error:', writeErr);
+      return;
+    }
+    console.log(`Bundle successfully written to ${outputFile}`);
+  });
+});
